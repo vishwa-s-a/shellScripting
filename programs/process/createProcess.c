d67b829ad17dadd0ef5eefa36b676fc18a443113
vishwa-s-a <vishwasappaji769@gmail.com>
2023-05-13 12:35:44 +0530

    mod createprocess.c

--- programs/process/createProcess.c
+++ programs/process/createProcess.c
@@ -5,6 +5,23 @@
 #include<unistd.h>
 
 int main(){
-    fork();
-    printf("Hello world\n");
+    // fork();
+    // printf("Hello world\n");
+    pid_t pid;
+    pid=fork();
+
+    if(pid<0)
+    {
+        printf("Process is not created\n");
+    }
+    else if(pid ==0)
+    {
+        printf("Child process created successfully\n");
+        printf("Child process id is %d and parent process id is %d\n", getpid(),getppid());
+    }
+    else{
+        printf("process id is %d and newly created process id is %d\n",getpid(),pid);
+    }
+
+    return 0;
 }
